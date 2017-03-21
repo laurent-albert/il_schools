a39bb8272af8106bcd13c357558217e33536e5ac
Laurent Albert <laurent@eeee.com>
2017-03-21 04:33:25 -0700

    applicant import

---
+++ il_schools/il_schools/doctype/il_student_applicant_import/il_student_applicant_import.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, IL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('il_student_applicant_import', {
+	refresh: function(frm) {
+
+	}
+});
