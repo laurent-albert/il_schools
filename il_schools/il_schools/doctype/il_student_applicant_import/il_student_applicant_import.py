063a702172cd8d9c20bb5f6d3044b4134d03387b
Laurent Albert <laurent.albert.st@gmail.com>
2017-03-31 19:01:21 +0200

    next step

--- il_schools/il_schools/doctype/il_student_applicant_import/il_student_applicant_import.py
+++ il_schools/il_schools/doctype/il_student_applicant_import/il_student_applicant_import.py
@@ -15,7 +15,7 @@ from frappe.utils import get_site_name, get_site_path, get_site_base_path, get_p
 #from DateTime import DateTime
 
 import csv
-from frappe.utils.csvutils import read_csv_content
+#from frappe.utils.csvutils import read_csv_content
 
 class ILStudentApplicantImport(Document):
 	def import_sa_file(self):
@@ -27,7 +27,7 @@ class ILStudentApplicantImport(Document):
 #					IdCandidat, Civilite, Nom, NomUsage
 					['first_name'],['middle_name'],['social_security_number'],['ine_number'],
 #					Prenom, Prenom2, NumSecu, NumIne
-					[],[],
+					['already_in_this_school'],[],
 #					EtudiantLimayrac, Photo
 					['date_of_birth','d','yyyy-mm-dd'],['place_of_birth'],
 #					DateNaissance, LieuNaissance
@@ -37,21 +37,19 @@ class ILStudentApplicantImport(Document):
 #					Adresse2, AdresseCp
 					['city'],['country'],
 #					AdresseVille, Pays
-#Pays;Tel;Tel2;Email;MotdePasse;DateCreationCompte;Session; Formation;RefCandidature;DateCandidature;EtatInscription;Scolarite_Annees1;Scolarite_Niveau1;Scolarite_Etablissement1;Scolarite_Ville1;Scolarite_Annees2;Scolarite_Niveau2;Scolarite_Etablissement2;Scolarite_Ville2;Scolarite_Annees3;Scolarite_Niveau3;Scolarite_Etablissement3;Scolarite_Ville3;Diplome_Annee1;Diplome_Libelle1;Diplome_Filiere1;Diplome_Annee2;Diplome_Libelle2;Diplome_Filiere2;Diplome_Annee3;Diplome_Libelle3;Diplome_Filiere3;Langue1;Langue2;Langue3;Langue4;ConditionPaiement;Iban;Bic;Banque;AdresseBanque;CpBanque;VilleBanque;QualiteRespFinancier;Autre_Lien;Nom;Prenom;Adresse;Cp;Ville;Pays;Tel;Tel2;TelTravail;Email;SituationFamiliale;SituationPro;Profession;Entreprise;AdresseEntreprise;CpEntreprise;VilleEntreprise;Contact_Pere_Nom;Contact_Pere_Prenom;Contact_Pere_Adresse;Contact_Pere_Adresse;Contact_Pere_Cp;Contact_Pere_Ville;Contact_Pere_Pays;Contact_Pere_Tel;Contact_Pere_Tel2;Contact_Pere_TelTravail;Contact_Pere_Email;Contact_Pere_SituationFamiliale;Contact_Pere_SituationPro;Contact_Pere_Entreprise;Contact_Pere_AdresseEntreprise;Contact_Pere_CpEntreprise;Contact_Pere_VilleEntreprise;Contact_Mere_Nom;Contact_Mere_Prenom;Contact_Mere_Adresse;Contact_Mere_Adresse;Contact_Mere_Cp;Contact_Mere_Ville;Contact_Mere_Pays;Contact_Mere_Tel;Contact_Mere_Tel2;Contact_Mere_TelTravail;Contact_Mere_Email;Contact_Mere_SituationFamiliale;Contact_Mere_SituationPro;Contact_Mere_Entreprise;Contact_Mere_AdresseEntreprise;Contact_Mere_CpEntreprise;Contact_Mere_VilleEntreprise;
-
-# 					["Ville de naissance",""],["Département de naissance",""],
-					["Pays de naissance",""],
-					["Nationalité",""],["Adresse 1","address_line_1"],
-					["Adresse 2","address_line_2"],["Adresse 3",""],
-					["Code postal","pincode"],["Commune","city"],
-					["Pays",""],["Téléphone",""],["Téléphone portable",""]]
+					[],['student_mobile_number'],
+#					Tel, Tel2
+					['student_email_id'],[],
+#					Email, MotdePasse
+#DateCreationCompte;Session; Formation;RefCandidature;DateCandidature;EtatInscription;Scolarite_Annees1;Scolarite_Niveau1;Scolarite_Etablissement1;Scolarite_Ville1;Scolarite_Annees2;Scolarite_Niveau2;Scolarite_Etablissement2;Scolarite_Ville2;Scolarite_Annees3;Scolarite_Niveau3;Scolarite_Etablissement3;Scolarite_Ville3;Diplome_Annee1;Diplome_Libelle1;Diplome_Filiere1;Diplome_Annee2;Diplome_Libelle2;Diplome_Filiere2;Diplome_Annee3;Diplome_Libelle3;Diplome_Filiere3;Langue1;Langue2;Langue3;Langue4;ConditionPaiement;Iban;Bic;Banque;AdresseBanque;CpBanque;VilleBanque;QualiteRespFinancier;Autre_Lien;Nom;Prenom;Adresse;Cp;Ville;Pays;Tel;Tel2;TelTravail;Email;SituationFamiliale;SituationPro;Profession;Entreprise;AdresseEntreprise;CpEntreprise;VilleEntreprise;Contact_Pere_Nom;Contact_Pere_Prenom;Contact_Pere_Adresse;Contact_Pere_Adresse;Contact_Pere_Cp;Contact_Pere_Ville;Contact_Pere_Pays;Contact_Pere_Tel;Contact_Pere_Tel2;Contact_Pere_TelTravail;Contact_Pere_Email;Contact_Pere_SituationFamiliale;Contact_Pere_SituationPro;Contact_Pere_Entreprise;Contact_Pere_AdresseEntreprise;Contact_Pere_CpEntreprise;Contact_Pere_VilleEntreprise;Contact_Mere_Nom;Contact_Mere_Prenom;Contact_Mere_Adresse;Contact_Mere_Adresse;Contact_Mere_Cp;Contact_Mere_Ville;Contact_Mere_Pays;Contact_Mere_Tel;Contact_Mere_Tel2;Contact_Mere_TelTravail;Contact_Mere_Email;Contact_Mere_SituationFamiliale;Contact_Mere_SituationPro;Contact_Mere_Entreprise;Contact_Mere_AdresseEntreprise;Contact_Mere_CpEntreprise;Contact_Mere_VilleEntreprise;
+					[]]
 
 					
 
 		file_path = os.getcwd()+get_site_path()[1:].encode('utf8') + self.import_file
 
 		with open(file_path, 'r') as csvfile:
-			content = read_csv_content(csvfile.read())
+			content = il_read_csv_content(csvfile.read())
 
 #		wb = load_workbook(filename=file_path, read_only=True)
 #		ws = wb.active
@@ -67,7 +65,7 @@ class ILStudentApplicantImport(Document):
 				new_doc = frappe.new_doc("IL Student Applicant")
 				new_doc.program = "BTS Économie Sociale Familiale"
 				for j, value in enumerate(rData):
-					if j < 18 and columCSV[j]:
+					if j < len(columCSV) and columCSV[j]:
 #						new_doc.last_name = cell.value
 						if len(columCSV[j]) <= 1:
 							setattr(new_doc, columCSV[j][0], value)
@@ -101,3 +99,44 @@ class ILStudentApplicantImport(Document):
 				frappe.db.rollback()
 			else:
 				frappe.db.commit()
+
+def il_read_csv_content(fcontent, ignore_encoding=False):
+	rows = []
+
+	if not isinstance(fcontent, unicode):
+		decoded = False
+		for encoding in ["utf-8", "windows-1252", "windows-1250"]:
+			try:
+				fcontent = unicode(fcontent, encoding)
+				decoded = True
+				break
+			except UnicodeDecodeError:
+				continue
+
+		if not decoded:
+			frappe.msgprint(_("Unknown file encoding. Tried utf-8, windows-1250, windows-1252."),
+				raise_exception=True)
+
+	fcontent = fcontent.encode("utf-8").splitlines(True)
+
+	try:
+		rows = []
+		for row in csv.reader(fcontent):
+			r = []
+			for val in row:
+				# decode everything
+				val = unicode(val, "utf-8").strip()
+
+				if val=="":
+					# reason: in maraidb strict config, one cannot have blank strings for non string datatypes
+					r.append(None)
+				else:
+					r.append(val)
+
+			rows.append(r)
+
+		return rows
+
+	except Exception:
+		frappe.msgprint(_("Not a valid Comma Separated Value (CSV File)"))
+		raise
